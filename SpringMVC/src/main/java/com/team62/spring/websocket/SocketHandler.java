54fa8582133e6985e9ad99f0c8b018a59fe80eb4
Luvai Hassanali <luvaihassanali@cmail.carleton.ca>
2018-03-05 18:36:57 -0500

    update

--- SpringMVC/src/main/java/com/team62/spring/websocket/SocketHandler.java
+++ SpringMVC/src/main/java/com/team62/spring/websocket/SocketHandler.java
@@ -3,14 +3,12 @@ package com.team62.spring.websocket;
 
 import java.io.IOException;
 import java.util.ArrayList;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import com.team62.spring.model.Player;
 import com.team62.spring.model.AI.AI;
 import com.team62.spring.model.adventureCards.AdventureCard;
@@ -18,7 +16,7 @@ import com.team62.spring.model.Game;
 
 @Component
 public class SocketHandler extends TextWebSocketHandler {
-	
+	private static final Logger logger = LogManager.getLogger(SocketHandler.class);
 	public Game gameEngine = new Game();
 	int sessionTracker = 0;
 
@@ -31,13 +29,15 @@ public class SocketHandler extends TextWebSocketHandler {
 			// first time connection
 
 			if (clientMessage.equals("Player attempting to connect")) {
-				System.out.println(clientMessage);
-				System.out.println("Player #" + session.getId() + " connected");
+				logger.info("Player attempting to connect...");
+				
                 if(gameEngine.players.size() == 4) {
                 	session.sendMessage(new TextMessage("Too many players, sorry. Being disconnected."));
+                	logger.info("A player got denied entry to game, already full");
                 	session.close();
                 	return;
                 } else {
+                	logger.info("Player #{} connected", session.getId());
                 	session.sendMessage(new TextMessage("Welcome, enter your name"));
                 }
 
@@ -47,16 +47,19 @@ public class SocketHandler extends TextWebSocketHandler {
 			if(clientMessage.equals("flipStoryDeck")) {
 				gameEngine.storyDeck.flipCard();
 				session.sendMessage(new TextMessage("flipStoryDeck" + gameEngine.storyDeck.faceUp.StringFile));
+				logger.info("Flipping card from story deck");
 			}
 			// initialize client
 			if (clientMessage.startsWith("Name")) {
 				String Name = clientMessage.substring(clientMessage.indexOf(':')+1);
 				Player clientObject = new Player(session.getId(), Name, session, sessionTracker);
+				logger.info("Player {} is enrolled in the quest", Name);
 				gameEngine.players.add(clientObject);
 				sessionTracker++;
 				session.sendMessage(new TextMessage("You are all set up, waiting for other players to connect.."));
 				//all clients have joined
 				if(gameEngine.players.size()==4) {
+					logger.info("All 4 players connected, starting game -> flipping first story card");
 					sendToAllSessions(gameEngine.players,"All players have joined, starting game...");
 					gameEngine.storyDeck.flipCard();
 					sendToAllSessions(gameEngine.players,"flipStoryDeck" + gameEngine.storyDeck.faceUp.StringFile);
@@ -80,13 +83,18 @@ public class SocketHandler extends TextWebSocketHandler {
 						gameEngine.players.get(i).session.sendMessage(new TextMessage("You are " + gameEngine.players.get(i).name));
 					}
 				}
-				System.out.println("\nADVENTURE DECK\n");
+				
+				logger.info("ADVENTURE DECK PROOF:");
 				for(AdventureCard a: gameEngine.adventureDeck.cards) {
-					System.out.println(a.name);					
+					int i=0;
+					logger.info("{}. {}",i,a.name);
+					i++;
 				}
-				System.out.println("\nSTORY DECK\n");
+				logger.info("STORY DECK PROOF:");
 				for(AdventureCard a: gameEngine.adventureDeck.cards) {
-					System.out.println(a.name);					
+					int i=0;
+					logger.info("{}. {}",i,a.name);
+					i++;				
 				}
 			}
 			
